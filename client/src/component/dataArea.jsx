ed18569db757e4d1f685034f6ee87596279bac6f
Elli-Jeon <68575268+Elli-Jeon@users.noreply.github.com>
2021-06-24 20:34:36 +0900

    [Fix] Change fetch function to useEffect

--- client/src/component/dataArea.jsx
+++ client/src/component/dataArea.jsx
@@ -1,19 +1,20 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import styles from './dataArea.module.css'
 
 function DataArea() {
     const [ data, setData ] = useState(null);
+    const [ keyword, setKeyword ] = useState('');
+    const [ search, setSearch ] = useState("");
     const [ loading, setLoading ] = useState(false);
-    const [ error, setError ] = useState(null);
-    const movieInput = useRef();
+    const [ error, setError ] = useState("");
 
-    const searchMovie = (e) => {
-        e.preventDefault();
-        const fetchData = async (keyword) => {
+    
+    useEffect(()=>{
+        const fetchData = async (search) => {
             try {
-                setError(null);
                 setData(null);
+                setError("");
                 setLoading(true);
                 const url = "/v1/search/movie.json";
                 const response = await axios.get(url, {
@@ -22,28 +23,29 @@ function DataArea() {
                         "X-Naver-Client-Secret" : process.env.REACT_APP_CLIENT_SECRET,
                     },
                     params : {
-                        query : `${keyword}`
+                        query : `${search}`
                     }
                 });
-                setData(response.data);
+                setData(response.data.items);
+                setLoading(false);
             } catch(error){
                 setError(error);
             }
-            setLoading(false);
         }
-        fetchData(movieInput.current.value);
-    }
-
-    useEffect(()=>{},[]);
-
+        search === "" ? console.log("아직 빈칸") : fetchData(search);
+    },[search]);
     console.log(data);
 
     return (
         <div>
             <div className={styles.searchBox}>
-                <input type="text" ref={movieInput}/>
-                <button type="submit" onClick={searchMovie}>Search</button>
+                <input type="text" value={keyword} onChange={(e)=>{setKeyword(e.target.value)}}/>
+                <button type="submit" onClick={()=>{setSearch(keyword)}}>Search</button>
             </div>
+            {error && <div>에러가 발생했습니다 : {error}</div>}
+            {loading?(<div>Loading...</div>):(
+                data
+            )}
         </div>
     )
 }
