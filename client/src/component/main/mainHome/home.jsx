eac5986ec926938c15e78d9c6d1ae0080ac3b937
Elli-Jeon <68575268+Elli-Jeon@users.noreply.github.com>
2021-06-28 15:58:46 +0900

    [Add] Fetching popular movie data from TMDB

--- client/src/component/main/mainHome/home.jsx
+++ client/src/component/main/mainHome/home.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link, Route,  BrowserRouter as Router } from 'react-router-dom'
-import DataArea from '../searchMovie/dataArea'
 import styles from './home.module.css'
 
 function Home() {
-   
+    
+    const [ data, setData ] = useState([]);
+    const [ error, setError ] = useState();
+    const [ loading, setLoading ] = useState(false);
+    
+    useEffect(()=>{
+        const fetchData = async() => {
+            try{
+                setData(null);
+                setError(null);
+                setLoading(true);
+                const response = await axios.get('/movie/popular',{
+                    params : {
+                        api_key : process.env.REACT_APP_TMDB_API_KEY,
+                        language : 'ko',
+                    }
+                })
+                setData(response.data.results);
+                setLoading(false);
+            }catch(error){
+                console.error(error);
+            }
+        }
+        fetchData();
+    },[])
+
+    //console.log(data);
+
     return (
         <Router>
             <div>
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -6,5 +6,11 @@ module.exports = function(app){
             target: "https://openapi.naver.com",
             changeOrigin: true
         })
+    );
+    app.use(
+        createProxyMiddleware('/movie/popular',{
+            target : 'https://api.themoviedb.org/3',
+            changeOrigin : true
+        })
     )
 };
